cbd365bdbb78a7068ee812c993350ae8d2eb400b
Pierpaolo Busacchi <pierbus.dev@gmail.com>
2021-06-23 17:43:47 +0200

    removed most old and unused components

--- src/containers/MockComponent/MockComponentStyle.js
+++
@@ -1,9 +0,0 @@
-const mockComponentStyle = theme => ({
-    paper: {
-        display: 'flex',
-        flexGrow: 1,
-        margin: theme.spacing(1)
-    }
-})
-
-export default mockComponentStyle
--- src/utilities/backend/socketio-helper.js
+++
@@ -1,7 +0,0 @@
-import io from 'socket.io-client';
-
-import config from '../../config';
-
-const socket = io(config.network.socketserver['url']);
-
-export default socket;
